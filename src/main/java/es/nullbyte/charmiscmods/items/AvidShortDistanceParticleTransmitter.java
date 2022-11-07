22130307cadb799c3733748eb69ec095948d4201
charliecharx <itsthecharliechar@gmail.com>
2022-11-07 13:14:51 +0100

    working ASDT

---
+++ src/main/java/es/nullbyte/charmiscmods/items/AvidShortDistanceParticleTransmitter.java
@@ -0,0 +1,9 @@
+package es.nullbyte.charmiscmods.items;
+
+import net.minecraft.world.item.Item;
+
+public class AvidShortDistanceParticleTransmitter extends Item {
+    public AvidShortDistanceParticleTransmitter(Properties properties) {
+        super(properties);
+    }
+}
