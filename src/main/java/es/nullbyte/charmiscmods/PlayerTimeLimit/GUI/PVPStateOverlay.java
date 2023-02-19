2f0eb7b4648282b9dee19a2574ccb5163c60be3c
charliecharx <itsthecharliechar@gmail.com>
2023-02-19 15:20:16 +0100

    WOrking rendering base GUI

--- src/main/java/es/nullbyte/charmiscmods/PlayerTimeLimit/GUI/PVPStateOverlay.java
+++ src/main/java/es/nullbyte/charmiscmods/PlayerTimeLimit/GUI/PVPStateOverlay.java
@@ -1,26 +1,73 @@
 package es.nullbyte.charmiscmods.PlayerTimeLimit.GUI;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+
+import static es.nullbyte.charmiscmods.CharMiscModsMain.MOD_ID;
 
 public class PVPStateOverlay{
     //if you need custom textures they should be placed unde resources/assets/yourmodid/textures/gui (gui interchangable with other folders)
     //https://www.youtube.com/watch?v=J3a7JT0rxTM
-
-    public static final IGuiOverlay mainoverlay = ((gui, poseStack, partialTick, width, height) -> {
+    //Refer to ClientEvents.java for the gui registration
+    private static final ResourceLocation TEXTURE = new ResourceLocation(MOD_ID, "textures/mainvisor/fhcmclogo.png");
+    public static final IGuiOverlay HUD_MAINOVERLAY = ((gui, poseStack, partialTick, width, height) -> {
         int x = width / 2;
         int y = height;
+        int logoxoffset = 155;
+        int logoyoffset = -200;
+        int rectanglexoffset = -25;
+        int rectangleyoffset = 0;
+        int componentWidth = 50;
+        int componentHeight = 150;
+        int textxoffset = 12;
+        int textyoffset = 20;
+        // Draw semi-transparent grey rectangle
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        RenderSystem.setShaderColor(0.5F, 0.5F, 0.5F, 0.5F);
+        GuiComponent.fill(poseStack, x + logoxoffset + rectanglexoffset, y + logoyoffset + rectangleyoffset, x + logoxoffset + componentWidth, y + logoyoffset + componentHeight, 0xFFFFFFFF);
 
+        // Draw "Time remaining" text
+        Minecraft minecraft = Minecraft.getInstance();
+        Font font = minecraft.font;
+        String timeRemaining = "Tiempo restante:";
+        int textWidth = font.width(timeRemaining);
+        int textX = x + logoxoffset + textxoffset - textWidth / 2;
+        int textY = y + logoyoffset + textyoffset;
+        font.draw(poseStack, timeRemaining, textX, textY, 0xFFFFFFFF);
+
+        // Draw "HH:MM:SS" text
+        String remainingTime = "HH:MM:SS"; // replace with your logic to get the remaining time
+        textWidth = font.width(remainingTime);
+        textX = x + logoxoffset + textxoffset - textWidth / 2;
+        textY += font.lineHeight + 2; // add some space between the two lines of text
+        font.draw(poseStack, remainingTime, textX, textY, 0xFFFFFFFF);
+
+        // Draw PVP State text
+        String pvpstate = "PVP:"; // replace with your logic to get the remaining time
+        textWidth = font.width(pvpstate);
+        textX = x + logoxoffset + textxoffset - textWidth / 2;
+        textY += font.lineHeight + 8; // add some space between the two lines of text
+        font.draw(poseStack, pvpstate, textX, textY, 0xFFFFFFFF);
+
+        // Draw "HH:MM:SS" text
+        String statePVP = "HARDCORE"; // replace with your logic to get the remaining time
+        textWidth = font.width(statePVP);
+        textX = x + logoxoffset + textxoffset - textWidth / 2;
+        textY += font.lineHeight + 2; // add some space between the two lines of text
+        font.draw(poseStack, statePVP, textX, textY, 0xFFFFFFFF);
+
+        //Render the logo
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        //draw a grey semitransparent rectangle to the right of the screen
-        GuiComponent.fill(poseStack, x, 0, width, height, 0x7F000000);
-        //Draw text on top that writes the remaining time
-        gui.getMinecraft().font.draw(poseStack, "HH:MM_SS", x, y, 0xFFFFFF);
-        //Below the tezt, draw more text that says "PVP is active"
-        gui.getMinecraft().font.draw(poseStack, "PVP is active", x, y + 10, 0xFFFFFF);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        GuiComponent.blit(poseStack, x + logoxoffset, y + logoyoffset, 0, 0, 25, 25,
+                25, 25);
+
     });
 }
--- src/main/java/es/nullbyte/charmiscmods/events/ClientEvents.java
+++ src/main/java/es/nullbyte/charmiscmods/events/ClientEvents.java
@@ -1,28 +1,28 @@
 package es.nullbyte.charmiscmods.events;
 
 import es.nullbyte.charmiscmods.CharMiscModsMain;
+import es.nullbyte.charmiscmods.PlayerTimeLimit.GUI.PVPStateOverlay;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import static es.nullbyte.charmiscmods.CharMiscModsMain.MOD_ID;
-import static es.nullbyte.charmiscmods.PlayerTimeLimit.GUI.PVPStateOverlay.mainoverlay;
+import static es.nullbyte.charmiscmods.PlayerTimeLimit.GUI.PVPStateOverlay.HUD_MAINOVERLAY;
 
 public class ClientEvents {
-    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
-    public static class ClientForgeEvents {
+    //@Mod.EventBusSubscriber(modid = CharMiscModsMain.MOD_ID, value = Dist.CLIENT)
+   // public static class ClientForgeEvents {
 
-    }
+   // }
 
-    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = CharMiscModsMain.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModBusEvents {
+
+        //Both the id of the hud and the path to gui resource must contain no uppercase letters or speciañ characters other than underscore
         @SubscribeEvent
-        public static void registerGuiOverlay(RegisterGuiOverlaysEvent event) {
-            // Register the renderers
-            event.registerAboveAll("pvpfullstate", mainoverlay);
+        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event) {
+            event.registerAboveAll("mainoverlayid", PVPStateOverlay.HUD_MAINOVERLAY);
         }
     }
-
-
 }
