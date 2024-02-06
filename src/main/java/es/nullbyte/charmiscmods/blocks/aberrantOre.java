1cf19ac8b3f1da0726e58f156bbdca224b3f2305
Charx <itsthecharliechar@gmail.com>
2024-02-06 22:59:12 +0100

    Tooltip fore aberrant ore

--- src/main/java/es/nullbyte/charmiscmods/blocks/aberrantOre.java
+++ src/main/java/es/nullbyte/charmiscmods/blocks/aberrantOre.java
@@ -1,14 +1,17 @@
 package es.nullbyte.charmiscmods.blocks;
 
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import net.minecraft.network.chat.Component;
+import java.awt.*;
+import java.util.List;
 
 public class aberrantOre extends Block {
 
@@ -17,4 +20,10 @@ public class aberrantOre extends Block {
     }
 
 
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter blocKGetter, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("block.chrmscmds.aberrant_ore.tooltip").withStyle(net.minecraft.ChatFormatting.DARK_PURPLE).withStyle(net.minecraft.ChatFormatting.ITALIC));
+        super.appendHoverText(pStack, blocKGetter, pTooltipComponents, pIsAdvanced);
+    }
 }
