02f2c9941a6d3c905d88a8b35682b4d861f23d67
Charx <itsthecharliechar@gmail.com>
2024-02-21 22:42:20 +0100

    Functioning multiloadrer archloom - to check

---
+++ common/src/main/java/es/nullbyte/relativedimensions/___AndRelativeDimensionsInMinecraft.java
@@ -0,0 +1,10 @@
+package es.nullbyte.relativedimensions;
+
+public class ___AndRelativeDimensionsInMinecraft
+{
+	public static final String MOD_ID = "relativedimensions";
+
+	public static void init() {
+		
+	}
+}
---
+++ fabric/src/main/java/es/nullbyte/relativedimensions/fabric/AndRelativeDimensionsInMinecraftFabric.java
@@ -0,0 +1,11 @@
+package es.nullbyte.relativedimensions.fabric;
+
+import es.nullbyte.relativedimensions.___AndRelativeDimensionsInMinecraft;
+import net.fabricmc.api.ModInitializer;
+
+public class AndRelativeDimensionsInMinecraftFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ___AndRelativeDimensionsInMinecraft.init();
+    }
+}
